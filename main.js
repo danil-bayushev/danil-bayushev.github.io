cac37a061ab745bc01c88cbd61612c92c56e15cf
Danil Bayushev <danil.bayushev@gmail.com>
2021-09-20 13:52:28 +0300

    commit

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ window.onload = function(){
 
    document.querySelector('.closeBtn').addEventListener('click', clear);
 
+   console.log(window.screen.height);
+
     function showFileInput(){
         fileO.click();
     }
